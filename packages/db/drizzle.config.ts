2e573e609884265c9ab5b1c5109f87a7220b657e
Hannes Dahlvik <hannes.dahlvik@gmail.com>
2023-12-06 17:29:08 +0200

    fix: Disable drizzle strict config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     schema: './src/schema.ts',
     out: './drizzle',
     driver: 'mysql2',
-    strict: true,
     verbose: true,
     dbCredentials: {
         uri: env.DATABASE_URL
