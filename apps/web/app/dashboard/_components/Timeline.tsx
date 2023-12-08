279340e9f8c9d7e30817ec4ac4dcb9989553d72a
Hannes Dahlvik <hannes.dahlvik@gmail.com>
2023-12-08 17:28:55 +0200

    feat(web): Dashboard timeline

---
+++ apps/web/app/dashboard/_components/Timeline.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+
+import { dayjs } from '@sathene/dayjs'
+
+export function DashboardTimeline() {
+    const hours = Array.from<number>({ length: 24 }).fill(0)
+    const hourBoxHeight = 125
+
+    const wrapper = useRef<HTMLDivElement>(null)
+    const hasRun = useRef(false)
+
+    const [needlePosition, setNeedlePosition] = useState(125 * 24)
+
+    useEffect(() => {
+        calcNeedlePos()
+        const interval = setInterval(() => calcNeedlePos(), 1000)
+        return () => clearInterval(interval)
+    }, [])
+
+    useEffect(() => {
+        if (!hasRun.current && needlePosition !== 0) {
+            hasRun.current = true
+            if (wrapper.current) {
+                wrapper.current.scrollTo({
+                    top: needlePosition - 200,
+                    behavior: 'smooth'
+                })
+            }
+        }
+    }, [needlePosition])
+
+    const calcNeedlePos = () => {
+        const time = dayjs()
+        const hour = time.hour()
+        const minute = time.minute()
+
+        const hourPos = hour * hourBoxHeight
+        const minutePos = hourBoxHeight / (60 / minute)
+        const finalPos = hourPos + minutePos
+
+        setNeedlePosition(finalPos)
+    }
+
+    return (
+        <div
+            className={`relative grid grid-rows-[repeat(24,_${hourBoxHeight}px)] h-full w-full bg-secondary border-l rounded-tr-xl rounded-br-xl overflow-y-scroll`}
+            ref={wrapper}
+        >
+            {hours.map((_, i) => (
+                <div className="grid grid-cols-[75px_1fr]">
+                    <div className="h-full w-full p-2 border-r border-b text-center">
+                        <p>{i}:00</p>
+                    </div>
+                    <div className="h-full w-full p-2 border-r border-b"></div>
+                </div>
+            ))}
+
+            <div
+                className="absolute h-[1px] w-full bg-red-500"
+                style={{ top: needlePosition }}
+            ></div>
+        </div>
+    )
+}
--- apps/web/app/dashboard/overview/page.tsx
+++ apps/web/app/dashboard/overview/page.tsx
@@ -1,7 +1,13 @@
+import { DashboardTimeline } from '../_components/Timeline'
+
 export default function DashboardOverviewPage() {
     return (
-        <div className="h-full p-6">
-            <h2>Dashboard Overview Page</h2>
+        <div className="grid grid-cols-[1fr_375px] h-full">
+            <div className="w-full p-6">
+                <h2>Dashboard Overview Page</h2>
+            </div>
+
+            <DashboardTimeline />
         </div>
     )
 }
