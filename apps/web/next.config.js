f851c0a04d79a4e75e22a1ca1c99ba7e5fc6c80e
HannesDahlvik <hannes.dahlvik@gmail.com>
2024-03-02 16:37:27 +0200

    feat(api): Switch from oslo/password to argon2

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,10 +5,6 @@ const nextConfig = {
     staticPageGenerationTimeout: 180,
     experimental: {
         serverComponentsExternalPackages: ['oslo']
-    },
-    webpack: (config) => {
-        config.externals.push('@node-rs/argon2', '@node-rs/bcrypt')
-        return config
     }
 }
 
