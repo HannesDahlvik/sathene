2abd01ea31da0aced1ce2eb01e2312c99a78e4b9
Hannes Dahlvik <hannes.dahlvik@gmail.com>
2024-03-11 20:21:56 +0200

    feat: Google oAuth
     - Moved checking if existing account exists, creating new user and account rows and creating a new session to its own function

--- packages/api/src/routers/auth.ts
+++ packages/api/src/routers/auth.ts
@@ -4,10 +4,12 @@ import { auth } from '../auth/lucia'
 import { getServerSession } from '../auth/utils'
 import { authedProcedure, router } from '../trpc'
 import { oAuthGithubRouter } from './oAuth/github'
+import { oAuthGoogleRouter } from './oAuth/google'
 import { TRPCError } from '@trpc/server'
 
 export const authRouter = router({
     github: oAuthGithubRouter,
+    google: oAuthGoogleRouter,
     logout: authedProcedure.mutation(async ({ ctx }) => {
         const { session } = await getServerSession()
 
--- packages/api/src/routers/oAuth/google.ts
+++ packages/api/src/routers/oAuth/google.ts
@@ -2,19 +2,29 @@ import { cookies } from 'next/headers'
 
 import { env } from '@sathene/env'
 
-import { github } from '../../auth/utils'
+import { google } from '../../auth/utils'
 import { checkAndOrCreateAccount } from '../../lib/utils'
 import { procedure, router } from '../../trpc'
 import { TRPCError } from '@trpc/server'
-import { OAuth2RequestError, generateState } from 'arctic'
+import { generateCodeVerifier, OAuth2RequestError, generateState } from 'arctic'
 import { z } from 'zod'
 
-export const oAuthGithubRouter = router({
+export const oAuthGoogleRouter = router({
     login: procedure.query(async () => {
         const state = generateState()
-        const url = await github.createAuthorizationURL(state)
+        const codeVerifier = generateCodeVerifier()
+        const url = await google.createAuthorizationURL(state, codeVerifier, {
+            scopes: ['profile']
+        })
 
         cookies().set('state', state, {
+            path: '/',
+            secure: env.NODE_ENV === 'production',
+            httpOnly: true,
+            maxAge: 60 * 10
+        })
+
+        cookies().set('code_verifier', codeVerifier, {
             path: '/',
             secure: env.NODE_ENV === 'production',
             httpOnly: true,
@@ -32,26 +42,33 @@ export const oAuthGithubRouter = router({
         )
         .query(async ({ input }) => {
             const storedState = cookies().get('state')?.value ?? null
+            const storedCodeVerifier = cookies().get('code_verifier')?.value ?? null
 
-            if (!input.code || !input.state || !storedState || input.state !== storedState) {
+            if (!input.code || !storedState || !storedCodeVerifier || input.state !== storedState) {
                 throw new TRPCError({
                     code: 'BAD_REQUEST'
                 })
             }
 
             try {
-                const tokens = await github.validateAuthorizationCode(input.code)
-                const userResponse = await fetch('https://api.github.com/user', {
-                    headers: {
-                        Authorization: `Bearer ${tokens.accessToken}`
+                const tokens = await google.validateAuthorizationCode(
+                    input.code,
+                    storedCodeVerifier
+                )
+                const userResponse = await fetch(
+                    'https://openidconnect.googleapis.com/v1/userinfo',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${tokens.accessToken}`
+                        }
                     }
-                })
+                )
                 const user = await userResponse.json()
 
                 return await checkAndOrCreateAccount({
-                    providerId: 'github',
-                    providerUserId: user.id,
-                    username: user.login
+                    providerId: 'google',
+                    providerUserId: user.sub,
+                    username: user.name
                 })
             } catch (err: any) {
                 if (err instanceof OAuth2RequestError) {
